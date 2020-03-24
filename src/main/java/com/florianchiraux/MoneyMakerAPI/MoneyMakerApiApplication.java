4b1c24042a2dc91a79192f2ca17f56ab5523c100
FlorianChrx <florianchiro@hotmail.fr>
2020-03-25 00:57:48 +0100

    init

---
+++ src/main/java/com/florianchiraux/MoneyMakerAPI/MoneyMakerApiApplication.java
@@ -0,0 +1,13 @@
+package com.florianchiraux.MoneyMakerAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyMakerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyMakerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/florianchiraux/MoneyMakerAPI/MoneyMakerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.florianchiraux.MoneyMakerAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyMakerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
